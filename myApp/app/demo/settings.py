5625e2ac226446a3cd807cbd3c7de5e6cd4acd50
Megaa Lin <megaacc.lin@gmail.com>
2025-02-21 16:50:58 +0800

    Move some env definitions

--- myApp/app/demo/settings.py
+++ myApp/app/demo/settings.py
@@ -13,18 +13,18 @@ env = environ.Env()
 BASE_DIR = root()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = env.str('DEBUG', 'False') == 'True'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.environ.get('SQL_USER', 'user'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
-        'HOST': os.environ.get('SQL_HOST', 'localhost'),
-        'PORT': os.environ.get('SQL_PORT', '5432'),
+        'ENGINE': env.str('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': env.str('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': env.str('SQL_USER', 'user'),
+        'PASSWORD': env.str('SQL_PASSWORD', 'password'),
+        'HOST': env.str('SQL_HOST', 'localhost'),
+        'PORT': env.str('SQL_PORT', '5432'),
     }
 }
 
@@ -34,7 +34,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 
 ALLOWED_HOSTS = env.str('ALLOWED_HOSTS').split(',')
 
